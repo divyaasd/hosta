9128baa842f69cecc4e57542cdeb6be17e55bb58
divyakd1810 <asdivya1801@gmail.com>
2024-10-27 12:51:10 +0530

    first commit32

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,41 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const cors=require('cors')
-const empModel=require('./models/empModel')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const empModel = require('./models/empModel');
 
-const app=express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors(
-    {
-        origin: 'new-repo-host-uk4h.vercel.app', // Replace with your React app's Vercel URL
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],         
-        credentials: true                                  
-      }
-))
+app.use(cors({
+    origin: 'https://hosta-dc2r.vercel.app', // Make sure the URL is correct
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
-mongoose.connect('mongodb+srv://divyaas340:9HUarAV8IMS4JsYP@cluster0.u8iw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://divyaas340:dksO3oPDWw7UwXFP@cluster0.cauvy.mongodb.net/');
 
-app.post('/register',(req,res)=>{
+app.post('/register', (req, res) => {
     empModel.create(req.body)
-    .then(employees=>res.json(employees))
-    .catch(err=>console.log(err))
+        .then(employees => res.json(employees))
+        .catch(err => console.log(err));
+});
 
-})
-
-app.post('/login',(req,res)=>{
-    const{email,password}=req.body;
-    empModel.findOne({email:email})
-    .then(user=>{
-        if(user){
-            if(user.password===password){
-                res.json('success')
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    empModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json('success');
+                } else {
+                    res.json('password is incorrect');
+                }
+            } else {
+                res.json('no records');
             }
-            else{
-                res.json('password is incorrect')
-            }
-        }
-        else{
-            res.json('no records')
-        }
-    })
-})
+        });
+});
 
-app.listen(3001,()=>{
-    console.log('server is running');
-    
-})
+module.exports = app; // Export the app for Vercel
+
